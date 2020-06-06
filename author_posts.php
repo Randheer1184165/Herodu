9f9dea264da3afb8807692d6b0edce286af664f5
Randheer Kumar <65314108+Randheer1184165@users.noreply.github.com>
2020-06-07 01:26:37 +0530

    Update author_posts.php

--- author_posts.php
+++ author_posts.php
@@ -52,7 +52,7 @@ if (isset($_GET['p_id'])) {
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="post.php?p_id={$post_id}"><?php echo $post_title  ?></a>
+                    <a href="post.php?p_id=$post_id"><?php echo $post_id; ?>"<?php echo $post_title  ?></a>
                 </h2>
                 <p class="lead">
                     All Post by
