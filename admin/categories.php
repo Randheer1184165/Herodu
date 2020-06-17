c123165e56c75f64b50c3d3562567d40aa4345bc
Randheer Kumar <65314108+Randheer1184165@users.noreply.github.com>
2020-06-18 02:18:04 +0530

    Update categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -1,111 +1,142 @@
-<?php include "includes/admin_header.php"; ?>
-<?php include "functions.php"; ?>
-<?php ob_start(); ?>
+<?php include "includes/admin_header.php" ?>
+    
+<?php 
+
+
+    
+    
+?>
 
     <div id="wrapper">
+       
 
-
-
-
-
-<?php include "includes/admin_navigation.php";?>
-
-
-
-
-        <div id="page-wrapper">
-
-            <div class="container-fluid">
-
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            WELCOME TO ADMIN
-                            <small>Author</small>
-                        </h1>
-
-                     <div class="col-xs-6">
-                       
-
- 
-          <?php insert_categories(); ?>
-
-             
-                       
-
-                        <form action="" method="post"> 
-                            <div class="form-group">
-                                <label for="cat-title">Add categories</label>
-                                <input type="text" class="form-control" name="cat_title">
-                            </div>
-                            <div class="form-group">
-                                <input class="btn btn-primary"type="submit" name="submit" value="Add categories">
-                         </div>
-                        </form>  
-
-
-      <?php
-    if (isset($_GET['edit'])) {
-    $cat_id =escape($_GET['edit']);
-    include "includes/update_categories.php";
-     
         
+  
+
+        <!-- Navigation -->
+ 
+        <?php include "includes/admin_navigation.php" ?>
+        
+        
+    
+
+<div id="page-wrapper">
+
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+
+
+            <h1 class="page-header">
+                Welcome to admin
+                <small>Author</small>
+            </h1>
+
+
+            <div class="col-xs-6">
+            
+            <?php insert_categories();  ?>
+    
+    <form action="" method="post">
+      <div class="form-group">
+         <label for="cat-title">Add Category</label>
+          <input type="text" class="form-control" name="cat_title">
+      </div>
+       <div class="form-group">
+          <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+      </div>
+
+    </form>
+    
+    <?php // UPDATE AND INCLUDE QUERY
+
+    if(isset($_GET['edit'])) {
+    
+        $cat_id = $_GET['edit'];
+        
+        include "includes/update_categories.php";
+       
+    
+    }
+                
+                
+    ?>
+
+    
+    </div><!--Add Category Form-->
+
+            <div class="col-xs-6">
+    <table class="table table-bordered table-hover">
+      
+
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Category Title</th>
+            </tr>
+        </thead>
+        <tbody>
+
+        <?php 
+
+
+    $query = "SELECT * FROM heroku_597cf2e5c9cb274.categories";
+    $select_categories = mysqli_query($connection,$query);  
+
+    while($row = mysqli_fetch_assoc($select_categories)) {
+    $cat_id = $row['cat_id'];
+    $cat_title = $row['cat_title'];
+
+    echo "<tr>";
+        
+    echo "<td>{$cat_id}</td>";
+    echo "<td>{$cat_title}</td>";
+   echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+   echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+    echo "</tr>";
+
     }
 
 
 
 
-      ?>
-
-
-
-
-                     </div> <!--Add category Form-->
-
-                      <div class="col-xs-6">
-
-<!--
-<?php
-
-//$query= "SELECT * FROM categories LIMIT 3";
-//$select_categories = mysqli_query($connection, $query);
 ?>
--->
+        
 
+      
 
+        </tbody>
+    </table>
 
-               <table class="table table-bordered table-hover">
-                   <thead>
-                       <tr>
-                           <th>Id</th>
-                            <th>Category Title</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                            
-                       </tr>
-                   </thead>
-                   <tbody>
+                        
+                        
+                        
+                </div>        
 
-
-           <?php findAllCategories();?>
-
-           <?php deleteCategories();?>
-
-
-                   </tbody>
-               </table>
-                  
-
-
-                      
-                    </div>
-                </div>
-                <!-- /.row -->
 
             </div>
-            <!-- /.container-fluid -->
-
         </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+
+
+
+<?php 
+
+deleteCategories();
+
+ ?>
+
+  
+        
+     
+        
         <!-- /#page-wrapper -->
-<?php include "includes/admin_footer.php";?>
+        
+    <?php include "includes/admin_footer.php" ?>
